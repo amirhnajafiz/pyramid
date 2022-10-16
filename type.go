ad01854c4656e55a42e1bbc0c7e628381126a459
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-16 11:04:57 +0330

    add: generic in type module

--- type.go
+++ type.go
@@ -1,13 +1,13 @@
 package pyramid
 
 // An Item is something we manage in a priority queue.
-type item struct {
+type item[T any] struct {
 	// each item has a value of its own that could be anything.
-	value any
+	value T
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
 }
 
 // CompareFunction
 // is used to compare items in priority queue.
-type compareFunction func(any, any) bool
+type compareFunction[T any] func(T, T) bool
