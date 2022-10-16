47da7953fb9973cfe9b0e5993156f56693d75ab3
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-16 10:55:52 +0330

    add: types

--- type.go
+++ type.go
@@ -2,7 +2,12 @@ package pyramid
 
 // An Item is something we manage in a priority queue.
 type Item struct {
+	// each item has a value of its own that could be anything.
 	value any
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
 }
+
+// CompareFunction
+// is used to compare items in priority queue.
+type CompareFunction func(*Item, *Item) bool
