732b9cc6bb86620fc4409c007e5ccc6039be4189
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-16 10:46:20 +0330

    update: heap struct

--- heap.go
+++ heap.go
@@ -4,46 +4,52 @@ import "container/heap"
 
 // Heap
 // is a list of items.
-type Heap []*Item
+type Heap struct {
+	list            []*Item
+	compareFunction func(*Item, *Item) bool
+}
 
 // Len
 // returns the heap size.
 func (pq Heap) Len() int {
-	return len(pq)
+	return len(pq.list)
 }
 
 // Less
 // comparator function.
 func (pq Heap) Less(i, j int) bool {
-	return pq[i].value < pq[j].value
+	return pq.compareFunction(pq.list[i], pq.list[j])
 }
 
 // Swap
 // function for heap.
 func (pq Heap) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
+	pq.list[i], pq.list[j] = pq.list[j], pq.list[i]
+	pq.list[i].index = i
+	pq.list[j].index = j
 }
 
 // Push
 // new items to heap list.
 func (pq *Heap) Push(x any) {
-	n := len(*pq)
+	n := len(pq.list)
+
 	item := x.(*Item)
 	item.index = n
-	*pq = append(*pq, item)
+
+	pq.list = append(pq.list, item)
 }
 
 // Pop
 // extract one item from heap list.
 func (pq *Heap) Pop() any {
-	old := *pq
+	old := pq.list
 	n := len(old)
 	item := old[n-1]
+
 	old[n-1] = nil  // avoid memory leak
 	item.index = -1 // for safety
-	*pq = old[0 : n-1]
+	pq.list = old[0 : n-1]
 
 	return item
 }
