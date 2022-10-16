639078dccfd917b95af6023383e9b918778d31ab
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-16 14:36:28 +0330

    add: update simple example

---
+++ example/update/simple/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/amirhnajafiz/pyramid"
+)
+
+func main() {
+	h := pyramid.NewHeap[int](func(a any, b any) bool {
+		return a.(int) < b.(int)
+	})
+
+	special := 675
+
+	h.Push(special)
+
+	for i := 2; i < 100; i++ {
+		h.Push(i)
+	}
+
+	h.Update(special, 0, func(a any, b any) bool {
+		return a.(int) == b.(int)
+	})
+
+	for h.Length() > 0 {
+		fmt.Printf("%d\n", h.Pop().(int))
+	}
+}
